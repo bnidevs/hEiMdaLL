4f279eea9411e4d678f345bbfc16c5e878256d69
Tania Cao <tcaosu@stuy.edu>
2018-05-11 06:05:41 -0400

    upload PriorityQueue.java

---
+++ v1/TaniaCao/PriorityQueue.java
@@ -0,0 +1,13 @@
+public interface PriorityQueue {
+    // isEmpty method
+    public boolean isEmpty();
+    
+    // add method
+    public void add (String value);
+    
+    // peekMin method
+    public String peekMin();
+    
+    // removeMin method
+    public String removeMin();	
+}
